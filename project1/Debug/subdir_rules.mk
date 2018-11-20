366db3bae830fedd6631c6c65ba1780b6ebe88ba
wizz4rt <45040218+wizz4rt@users.noreply.github.com>
2018-11-20 11:23:50 +0100

    Update project1/Debug/subdir_rules.mk

--- project1/Debug/subdir_rules.mk
+++ project1/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.o: ../%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: GNU Compiler'
-	"C:/ti/ccsv8/tools/compiler/msp430-gcc-7.3.1.24_win32/bin/msp430-elf-gcc.exe" -c -mmcu=msp430g2553 -mhwmult=none -I"C:/ti/ccsv8/ccs_base/msp430/include_gcc" -I"C:/Users/Jan-Niclas Nutt/Desktop/sensor_analog" -I"C:/ti/ccsv8/tools/compiler/msp430-gcc-7.3.1.24_win32/msp430-elf/include" -Og -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"C:/ti/ccsv8/tools/compiler/msp430-gcc-7.3.1.24_win32/bin/msp430-elf-gcc.exe" -c -mmcu=msp430g2553 -mhwmult=none -I"C:/ti/ccsv8/ccs_base/msp430/include_gcc" -I"C:/Users/Jan-Niclas Nutt/Desktop/MSP_source_files/project1" -I"C:/ti/ccsv8/tools/compiler/msp430-gcc-7.3.1.24_win32/msp430-elf/include" -Og -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
