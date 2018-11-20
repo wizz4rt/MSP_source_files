0b85ed116c63fe9ec502f90af95943eb995f6750
wizz4rt <45040218+wizz4rt@users.noreply.github.com>
2018-11-20 12:12:48 +0100

    added spi class and header

---
+++ project1/spi.c
@@ -0,0 +1,14 @@
+/*
+ * spi.c
+ *
+ *  Created on: 20.11.2018
+ *      Author: Jan-Niclas Nutt
+ */
+
+#include "spi.h"
+
+void spi_init(void);
+{
+
+}
+
---
+++ project1/spi.h
@@ -0,0 +1,15 @@
+/*
+ * spi.h
+ *
+ *  Created on: 20.11.2018
+ *      Author: Jan-Niclas Nutt
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+
+
+
+
+#endif /* SPI_H_ */
